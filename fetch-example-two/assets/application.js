84ce5fbdb633538f862ce0efbc3813a0b5ec39e0
farrellw <farrell.will.g@gmail.com>
2019-10-10 18:29:51 -0500

    Update branch with in class work.

--- fetch-example-two/assets/application.js
+++ fetch-example-two/assets/application.js
@@ -1,5 +1,19 @@
 function init() {
   //  Get from the API "https://jsonplaceholder.typicode.com/comments"
+  console.log("Hello World");
+  fetch("https://jsonplaceholder.typicode.com/comments")
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(json) {
+      let commentsTag = document.getElementById("comments-list");
+
+      for (let i = 0; i < json.length; i++) {
+        let comment = json[i].body;
+        let commentTag = `<li>${comment}</li>`;
+        commentsTag.innerHTML += commentTag;
+      }
+    });
 }
 
 window.onload = init;
