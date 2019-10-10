229fb6fc2fcc2876c82b8785c582f2fdfde3c63b
farrellw <farrell.will.g@gmail.com>
2019-10-10 16:59:52 -0500

    Delete answer.

--- fetch-example-two/assets/application.js
+++ fetch-example-two/assets/application.js
@@ -1,22 +1,5 @@
 function init() {
-  fetch("https://jsonplaceholder.typicode.com/comments").then(function(
-    response
-  ) {
-    response.json().then(function(json) {
-      let div = document.getElementById("comments");
-      for (let i = 0; i < json.length; i++) {
-        div.innerHTML += `
-            <div class="comment" id="ex-comment-${json[i].id}">
-                <h1>${json[i].name}</h1>
-                <h3>Email: <a href="mailto:${json[i].email}">${json[i].email}</a></h3>
-                <p><strong>Comment:</strong> ${json[i].body}</p>
-            </div>
-          `;
-      }
-    });
-    // Add Code for retrieving comments using fetch() from https://jsonplaceholder.typicode.com/
-    // Then code code for adding the comment JSON values to the <div id="comments"> div using a for loop
-  });
+  //  Get from the API "https://jsonplaceholder.typicode.com/comments"
 }
 
 window.onload = init;
